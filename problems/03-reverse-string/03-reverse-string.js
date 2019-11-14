dfb860f332cc78c4065badff41fe6c9e32265609
sarithakarunanithi <sarithakumar06@gmail.com>
2019-11-14 13:47:18 -0500

    3-Reverse String Done

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,13 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
+    let rev = '';
+
+    for(let i = someStr.length-1; i >= 0; i--){
+        rev = rev + someStr.charAt(i);
+    }        
+    return rev;    
 }
+console.log(reverseStr('Saritha'));
+   
+
